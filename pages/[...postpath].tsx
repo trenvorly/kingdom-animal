f16291f3b737e6d857117c2bb2c929956825499c
trenvorly <131934149+trenvorly@users.noreply.github.com>
2023-05-12 16:21:20 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://www.trenvorly.site` + encodeURI(path as string)
+					`https://www.trenvorly.site/` + encodeURI(path as string)
 				}`,
 			},
 		};
